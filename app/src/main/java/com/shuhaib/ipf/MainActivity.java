115af146982d4c18e939cc5eb5b02a84fc60d6fd
ShuhaibNC <shuhaibsaad5@gmail.com>
2024-08-21 17:50:02 +0530

    Changed

--- app/src/main/java/com/shuhaib/ipf/MainActivity.java
+++ app/src/main/java/com/shuhaib/ipf/MainActivity.java
@@ -13,54 +13,68 @@ import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 import android.view.WindowManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MainActivity extends Activity { 
 
     public static TextView warnt;
-    TextView ipaddress;
-    Button restart;
+    public static TextView ipaddress;
+    public static Button restart;
+    public static String err;
      
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        
         ipaddress = findViewById(R.id.ipaddress);
         warnt = findViewById(R.id.warntext);
         restart = findViewById(R.id.restart);
-        
-        NetworkUtils nu = new NetworkUtils();
-        String ipaddr = nu.getLocalIpAddress();
-        try{
-            if (ipaddr == null){
-                ipaddress.setText("127.0.0.1");
-                warnt.setText("Please turn on mobile hotspot and press restart");
-        }
-            else{
-                ipaddress.setText(ipaddr);
-        }
-        }
-        catch(Exception e){
-            warnt.setText(e.getMessage());
-        }
+        refreshIP();
         restart.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View view) {
-                    restartApp(MainActivity.this);
+                    refreshIP();
                 }
             });
         
+        
     }
     
-    public static void restartApp(Activity activity) {
-        Intent intent = activity.getPackageManager()
-            .getLaunchIntentForPackage(activity.getPackageName());
+   
+    
+    public void refreshIP() {
+        NetworkUtils nu = new NetworkUtils();
+        final String ipaddr = nu.getLocalIpAddress();
+        
+        ipaddress.setOnClickListener(new View.OnClickListener() {
 
-        if (intent != null) {
-            activity.finish();
-            activity.startActivity(intent);
-        }
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    if (ipaddr != null){
+                    ClipData clip = ClipData.newPlainText("Copied Text", ipaddr);
+                    clipboard.setPrimaryClip(clip);}
+                    else{
+                        ClipData clip = ClipData.newPlainText("Copied Text", "127.0.0.1");
+                        clipboard.setPrimaryClip(clip);
+                    }
+                }
+            });
+        
+           if (ipaddr == null){
+               ipaddress.setText("127.0.0.1");
+               warnt.setText("Turn on mobile hotspot and press refresh");
+            }
+            else{
+                ipaddress.setText(ipaddr);
+                warnt.setText("");
+            }
+        
     }
 	
 } 
@@ -85,8 +99,9 @@ class NetworkUtils {
                     }
                 }
             }
-        } catch (Exception e) {
-            MainActivity.warnt.setText(e.getMessage());
+        }
+        catch (Exception e) {
+           return null;
         }
 
         return null;
